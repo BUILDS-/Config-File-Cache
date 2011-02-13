6ac5704f8f889933c025a112069feedd37d3a0a5
George Silvis III <georgeIII@GASilvis.net>
2011-02-13 15:23:23 -0500

    Fixed bug in mpdmonitor.sh with composer tag

--- gsilvis/mpdmonitor.sh
+++ gsilvis/mpdmonitor.sh
@@ -2,9 +2,9 @@
 
 # "##" escaped by mpc to "#"
 
-mpc current -f "[<fc=##06989A>%composer%</fc> - ]|[<fc=##06989A>%artist%</fc> - ]<fc=##C4A000>%title%</fc> - <fc=##CC0000>%album%</fc>"
+mpc current -f "[[<fc=##06989A>%composer%</fc> - ]|[<fc=##06989A>%artist%</fc> - ]]<fc=##C4A000>%title%</fc> - <fc=##CC0000>%album%</fc>"
 
 while [ 1 ]
 do
-  mpc current --wait -f "[<fc=##06989A>%composer%</fc> - ]|[<fc=##06989A>%artist%</fc> - ]<fc=##C4A000>%title%</fc> - <fc=##CC0000>%album%</fc>"
+  mpc current --wait -f "[[<fc=##06989A>%composer%</fc> - ]|[<fc=##06989A>%artist%</fc> - ]]<fc=##C4A000>%title%</fc> - <fc=##CC0000>%album%</fc>"
 done

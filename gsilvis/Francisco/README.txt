d1e74ba6b198910448c68940b4ebb21f8c3907f4
George Silvis, III <george.iii.silvis@gmail.com>
2011-06-27 23:09:23 -0400

    Reinstalled Francisco. Updates for that.

--- gsilvis/Francisco/README.txt
+++ gsilvis/Francisco/README.txt
@@ -1,7 +1,26 @@
-Welcome to Francisco.
+Welcome to Francisco. I finally reinstalled this machine. There is no longer a
+Windows partition, nor an Ubuntu one, nor the ridiculous setup I had with two
+separate storage partitions. Added this time is the fact that my root
+partition is encrypted, using LVM/dm-crypt/LUKS. Doing this with the installer
+is not too difficult, if you just have it do it all for you. It boots a little
+more slowly, though.
 
-.dzen -- this folder contains the stuff for my kind of weird status bar setup.
+Also different, I have decided to only install things with aptitude, this time.
+So, no crazy up-to-date xmobar or xmonad or anything for me. Things appear to
+work, though.
 
-These files are all pretty self-explanatory.
+What I have here:
+
+
+.emacs: Uh, what do you THINK this is?
+
+.xinitrc is run when my X session starts. Note the first part especially,
+which deals with a nasty nm-applet bug where it fails to save any wireless
+keys.
+
+.xmobarrc is for xmobar
 
 xmonad.hs belongs at ~/.xmonad/xmonad.hs
+
+
+.bashrc, .bash_aliases. Nothing complicated here.
--- gsilvis/Francisco/xmonad.hs
+++ gsilvis/Francisco/xmonad.hs
@@ -5,67 +5,43 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Scratchpad
-import XMonad.Hooks.UrgencyHook
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
 import System.IO 
  
+
 
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
     , className =? "Gimp"           --> doFloat
-    , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore 
-    , (className =? "google-chrome" <&&> resource =? "Dialog") --> doFloat
     , title     =? "Dwarf Fortress" --> doFloat
     , title     =? "Minecraft Launcher" --> doFloat
-    , scratchpadManageHookDefault
     ]
 
-myStatusBar = "dzen2 -x 0 -w 400 -ta l"
-myConkyBar = "sleep 1 && conky -c ~/.dzen/.conkydzenrc2 | dzen2 -x 400 -w 460  -ta r"
-myOtherConkyBar = "conky -c ~/.dzen/.conkyhelperrc | gdbar -s o -fg red | dzen2 -x 860 -w 90 -ta c"
-
-main = do
-  xmproc <- spawnPipe myStatusBar
-  bluh <- spawnPipe myConkyBar
-  bluh2 <- spawnPipe myOtherConkyBar
-  xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-    { terminal          = "gnome-terminal"
+main = do 
+  xmproc <- spawnPipe "xmobar /home/george/.xmobarrc"
+  xmonad $ defaultConfig
+    { terminal          = "xfce4-terminal"
     , focusFollowsMouse = False
     , modMask           = mod4Mask
-    , layoutHook        = smartBorders $ avoidStruts $ Full ||| (Tall 1 (3/100) (1/2))
+    , layoutHook        = avoidStruts $ layoutHook defaultConfig
     , manageHook        = manageDocks <+> myManageHook
-    , startupHook       = setWMName "LG3D"
-    , logHook           = let
-        shortenScratchpad x = case x of
-          "NSP" -> "N"
-          _     -> x
-        in dynamicLogWithPP $ dzenPP
-          { ppCurrent         = dzenColor "green"   "black" . shortenScratchpad
-          , ppUrgent          = dzenColor "yellow"  "red"   . shortenScratchpad
-          , ppHidden          = dzenColor "black"   "gray"  . shortenScratchpad
-          , ppHiddenNoWindows = dzenColor "gray"    "gray"  . shortenScratchpad
-          , ppVisible         = dzenColor "blue"    "black" . shortenScratchpad
-          , ppOutput          = hPutStrLn xmproc
-          }
+    , logHook           = dynamicLogWithPP $ xmobarPP
+        { ppOutput = hPutStrLn xmproc
+        , ppTitle  = xmobarColor "green" "" . shorten 50
+        }
     } `additionalKeys`
--- lock screen
-      [ ((mod4Mask    , xK_x    ), spawn "gnome-screensaver-command --lock")
 -- screen capture (full screen, and just active window. Save to ~/Pictures/Captures)
-      , ((0           , xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Captures/'")
+      [ ((0           , xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Captures/'")
       , ((controlMask , xK_Print), spawn "scrot -u -e 'mv $f ~/Pictures/Captures/'")
--- Scratchpad function. M-; hides or reveals it.
-      , ((mod4Mask    , xK_semicolon   ), scratchpadSpawnActionCustom "gnome-terminal --disable-factory --name scratchpad")
--- the following are for manipulating mpd, the music player daemon, my music player of choice.
-      , ((mod4Mask    , xK_equal       ), spawn "mpc volume +5")
-      , ((mod4Mask    , xK_minus       ), spawn "mpc volume -5")
-      , ((mod4Mask    , xK_backslash   ), spawn "mpc toggle")
-      , ((mod4Mask    , xK_bracketleft ), spawn "mpc seek 0%")
-      , ((mod4Mask    , xK_bracketright), spawn "mpc next")
+-- the following are for manipulating mpd, the music player daemon, my music player of choice
+      , ((mod4Mask    , xK_equal       ), spawn "ncmpcpp volume +5")
+      , ((mod4Mask    , xK_minus       ), spawn "ncmpcpp volume -5")
+      , ((mod4Mask    , xK_backslash   ), spawn "mpc -h \"/var/lib/mpd/socket\" toggle")
+      , ((mod4Mask    , xK_bracketleft ), spawn "ncmpcpp seek 0%")
+      , ((mod4Mask    , xK_bracketright), spawn "ncmpcpp next")
 -- the following are for common things that I want to do in a terminal
-      , ((mod4Mask .|. shiftMask, xK_z ), spawn "gnome-terminal -e ncmpcpp")
-      , ((mod4Mask .|. shiftMask, xK_l ), spawn "gnome-terminal -e alsamixer")
-      , ((mod4Mask    , xK_F11         ), spawn "gnome-terminal -e 'vim /home/george/.xmonad/xmonad.hs'")
+      , ((mod4Mask .|. shiftMask, xK_z ), spawn "xfce4-terminal -e ncmpcpp")
+      , ((mod4Mask .|. shiftMask, xK_l ), spawn "xfce4-terminal -e alsamixer")
+      , ((mod4Mask    , xK_F11         ), spawn "xfce4-terminal -e 'vim /home/george/.xmonad/xmonad.hs'")
+-- lock screen
+      , ((mod4Mask    , xK_x           ), spawn "gnome-screensaver-command --lock")
       ]

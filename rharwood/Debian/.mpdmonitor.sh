93f9767dd421f10870adf22686543a77d4153951
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2011-09-02 18:58:22 -0400

    .mpdmonitor.sh comes closer to a real client every day

--- rharwood/Debian/.mpdmonitor.sh
+++ rharwood/Debian/.mpdmonitor.sh
@@ -4,6 +4,11 @@
 
 while [ 1 ]
 do
-  nice -n 19 mpc --host /home/robbie/.mpd/.socket current --wait -f "%artist% - \"%title%\" #[%album%#]";
-  mpc --host /home/robbie/.mpd/.socket idle;
+  nice -n 19 mpc --host /home/frozencemetery/.mpd/socket current --wait -f "%artist% - \"%title%\" #[%album%#]";
+  mpc --host /home/frozencemetery/.mpd/socket idle;
+  if [ -z "$(mpc --host /home/frozencemetery/.mpd/socket playlist)" ]; then
+      echo "queueing";
+      mpc -h /home/frozencemetery/.mpd/socket add "$(cd /home/frozencemetery/Music; find -follow -mindepth 1 -type d | shuf | head -n1 | cut -b3- ; cd ->/dev/null)";
+      ncmpcpp play;
+  fi;
 done

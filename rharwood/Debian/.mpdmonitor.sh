644598c1692d3d5745581434f5075f38fcbd0aba
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2011-09-02 19:19:31 -0400

    bugs are hard

--- rharwood/Debian/.mpdmonitor.sh
+++ rharwood/Debian/.mpdmonitor.sh
@@ -5,10 +5,11 @@
 while [ 1 ]
 do
   nice -n 19 mpc --host /home/frozencemetery/.mpd/socket current --wait -f "%artist% - \"%title%\" #[%album%#]";
+  sleep 1;
   mpc --host /home/frozencemetery/.mpd/socket idle;
   if [ -z "$(mpc --host /home/frozencemetery/.mpd/socket playlist)" ]; then
       echo "queueing";
-      cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -type d  | grep "./" | shuf | head -n1)" -maxdepth 1 -type d | grep "./" | cut -b 1-2 --complement | grep "/" | shuf | head -n1 | mpc --host /home/frozencemetery/.mpd/socket add;
+      cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host /home/frozencemetery/.mpd/socket add;
       ncmpcpp play;
   fi;
 done

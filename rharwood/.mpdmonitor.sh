7430092d59d04432b9edc25af61728e787f28f53
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2012-05-28 16:31:54 -0400

    Colored .mpdmonitor.sh.
    Coloration is based on the default playing color in ncmpcpp in my terminal
    which uses the Zenburn scheme.

--- rharwood/.mpdmonitor.sh
+++ rharwood/.mpdmonitor.sh
@@ -2,13 +2,12 @@
 
 # "##" escaped by mpc to "#"
 
-while [ 1 ]
-do
-  nice -n 19 mpc --host /home/frozencemetery/.mpd/socket current --wait -f "%artist% - \"%title%\" #[%album%#]";
-  mpc --host /home/frozencemetery/.mpd/socket idle;
+while true; do
+  nice -n 19 mpc --host /home/frozencemetery/.mpd/socket current --wait -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]"
+  mpc --host /home/frozencemetery/.mpd/socket idle
   if [ -z "$(mpc --host /home/frozencemetery/.mpd/socket playlist)" ]; then
-      echo "queueing";
-      cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host /home/frozencemetery/.mpd/socket add;
-      ncmpcpp play;
-  fi;
+      echo "queueing"
+      cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host /home/frozencemetery/.mpd/socket add
+      mpc --host /home/frozencemetery/.mpd/socket play
+  fi
 done

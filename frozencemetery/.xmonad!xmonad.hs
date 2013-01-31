b2c3f52e66f7912f72ffd3bc5c48731f58810633
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2013-01-30 20:44:55 -0500

    Style cleanup.

--- frozencemetery/.xmonad!xmonad.hs
+++ frozencemetery/.xmonad!xmonad.hs
@@ -26,10 +26,14 @@ suckterm :: String -> X()
 suckterm x = spawn("urxvt -e sh -c 'sleep .07; exec " ++ x ++ "'")
 
 mpc :: String -> X ()
-mpc = \x -> spawn("MPD_HOST=/home/frozencemetery/.mpd/socket mpc " ++ x)
+mpc x = spawn("MPD_HOST=/home/frozencemetery/.mpd/socket mpc " ++ x)
 
 asroot :: String -> X ()
-asroot = \x -> spawnterm("su -c '" ++ x ++ "'")
+asroot x = spawnterm("su -c '" ++ x ++ "'")
+
+dmenu :: String -> String
+dmenu prompt = 
+  "dmenu -fn Terminus -nb black -nf grey -sb orange -sf black -p " ++ prompt
 
 widthdelta :: Rational
 widthdelta = 3/100
@@ -41,7 +45,7 @@ main = do
            , focusFollowsMouse = False
            , borderWidth = 1
            , modMask = mod4Mask
-           , workspaces = ["1","2","3","4","5","6","7","8","9","0"]
+           , workspaces = map show $ [1..9] ++ [0]
            , normalBorderColor = "#000000"
            , focusedBorderColor = "#ff7228"
            , keys =          
@@ -55,7 +59,10 @@ main = do
                 , ("M-m", windows W.focusMaster)
                 , ("M-t", withFocused $ windows . W.sink)
                 , ("M-S-q", io $ exitWith ExitSuccess)
-                , ("M-q", spawn "killall -g .mpdmonitor.sh ; killall -g .alsamonitor.sh; xmonad --recompile && xmonad --restart")
+                , ("M-q", spawn $
+                          "killall -g .mpdmonitor.sh ; killall -g .alsamonitor.sh;"
+                          ++ "xmonad --recompile && xmonad --restart"
+                  )
 
                 , ("M-b", sendMessage ToggleStruts)
 
@@ -64,9 +71,11 @@ main = do
                 , ("M-j", sendMessage MirrorShrink)
                 , ("M-k", sendMessage MirrorExpand)
 
-                , ("M-<Space>", spawn "exec \"$(dmenu_path | dmenu -fn Terminus -nb black -nf grey -sb orange -sf black -p cmd:)\"")
+                , ("M-<Space>", spawn $ 
+                                "exec \"$(dmenu_path | " 
+                                ++ dmenu "cmd:" ++ ")\"")
                 , ("M-u", spawn "pwman -l -x -s$(pwman -L | dmenu)")
-                , ("M-S-u", suckterm "emacsclient -nw -e \"(notmuch\"")
+                , ("M-S-u", suckterm "emacsclient -nw -e \"(notmuch)\"")
 
                 , ("M-<XF86AudioPrev>", mpc "pause")
                 , ("M-<XF86AudioPlay>", spawnterm "alsamixer")
@@ -80,8 +89,8 @@ main = do
                 , ("M-`", windows W.focusUp)
                 , ("M-S-`", windows W.swapUp)
 
-                , ("M-s", sendMessage NextLayout) -- switch between tall and grid
-                , ("M-S-s", setLayout $ XMonad.layoutHook conf) -- hard reset layout
+                , ("M-s", sendMessage NextLayout) -- tall <-> grid
+                , ("M-S-s", setLayout $ XMonad.layoutHook conf) -- reset
                 , ("M-f", sendMessage $ Toggle FULL)
                 , ("M-a", sendMessage $ Toggle MIRROR)
                 , ("M-S-f", windows W.swapMaster)
@@ -92,7 +101,8 @@ main = do
 
                 , ("M-v", spawn "echo \"\" | xsel -i && echo \"\" | xsel -ib")
 
-                , ("M-S-/", spawnterm "less /usr/share/X11/locale/en_US.UTF-8/Compose")
+                , ("M-S-/", spawnterm 
+                            "less /usr/share/X11/locale/en_US.UTF-8/Compose")
 
                 , ("M-x", spawn "xscreensaver-command --lock")
                 , ("M-S-x", spawn "killall vlock-main")
@@ -104,17 +114,21 @@ main = do
                 ]
                )
              `M.union`
-             (M.fromList 
-                 [((m .|. modm, k), windows $ f i)
-                  | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
-                 , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-                 ]
-             )
-             `M.union` 
-             (M.fromList
-              [((m .|. modm .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-               | (key, sc) <- zip [xK_2, xK_1, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0] [0..]
-              , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+             (M.fromList $
+              [ ((m .|. modm, k), windows $ f i) 
+                  | (i, k) <- zip (XMonad.workspaces conf) $ [xK_1 .. xK_9] ++ [xK_0]
+              , (f, m) <- [ (W.greedyView, 0)
+                          , (W.shift, shiftMask)
+                          ]
+              ]
+              ++
+              [ ( (m .|. modm .|. mod1Mask, key)
+                , screenWorkspace sc >>= flip whenJust (windows . f)) 
+                | (key, sc) <- zip (xK_2:xK_1:[xK_3..xK_9] ++ [xK_0]) [0..]
+              , (f, m) <- [ (W.view, 0)
+                          , (W.shift, shiftMask)
+                          ]
+              ]
              )
            , mouseBindings =
              \XConfig {XMonad.modMask = modm} ->
@@ -134,12 +148,12 @@ main = do
                                       )
                     )
                   ]
-           , layoutHook = avoidStruts $
-                          smartBorders $
-                          mkToggle (single FULL) . mkToggle (single MIRROR) $
-                          onWorkspace "0" (ResizableTall 1 (widthdelta) (4/5) [] ||| Grid) $
-                          onWorkspace "1" (ResizableTall 1 (widthdelta) (1/5) [] ||| Grid) $
-                          (ResizableTall 1 (widthdelta) (1/2) []) ||| Grid
+           , layoutHook = 
+             avoidStruts $ smartBorders $
+             mkToggle (single FULL) . mkToggle (single MIRROR) $
+             onWorkspace "0" (ResizableTall 1 widthdelta (4/5) [] ||| Grid) $
+             onWorkspace "1" (ResizableTall 1 widthdelta (1/5) [] ||| Grid) $
+             (ResizableTall 1 (widthdelta) (1/2) []) ||| Grid
            , logHook = dynamicLogWithPP $ xmobarPP 
                        { ppCurrent = xmobarColor "" "orange" . xmobarStrip
                        , ppVisible = xmobarColor "" "white" . xmobarStrip
@@ -149,28 +163,36 @@ main = do
                        , ppUrgent = xmobarColor "orange" "black" . xmobarStrip
                        , ppSep = " "
                        , ppTitle = const ""
-                       , ppLayout  = \a -> case a of
-                                             "Full" -> "ƒ"
-                                             "Grid" -> "g"
-                                             "Mirror Grid" -> "G"
-                                             "ResizableTall" -> "t"
-                                             "Mirror ResizableTall" -> "T"
-                                             _ -> a
-                       , ppExtras = [L.logCmd "echo \"| $(notmuch count tag:inbox):$(notmuch count tag:unread)\""]
-                       , ppOrder = \[workspaces, layout, title, mail] -> [workspaces, layout, title, mail]
-           , ppOutput = hPutStrLn xmproc
-           }
-           , manageHook = manageDocks <+> (composeAll
-                                           [ className =? "MPlayer" --> doFloat
-                                           , resource =? "Dialog" --> doFloat
-                                           , title =? "Pidgin" --> doFloat
-                                           , title =? "Dwarf Fortress" --> doFloat
-                                           , title =? "QEMU" --> doFloat -- this doesn't work
-                                           , className =? "Pidgin" --> doShift "1"
-                                           , className =? "Iceweasel" --> doShift "2"
-                                           , className =? "Icedove" --> doShift "3"
-                                           ]
-                                          )
+                       , ppLayout = \a -> case a of
+                                            "Full" -> "ƒ"
+                                            "Grid" -> "g"
+                                            "Mirror Grid" -> "G"
+                                            "ResizableTall" -> "t"
+                                            "Mirror ResizableTall" -> "T"
+                                            _ -> a
+                       , ppExtras = [L.logCmd $
+                                     "echo \"| `" ++ "notmuch count tag:inbox"
+                                     ++ "`:`" ++ "notmuch count tag:unread" 
+                                     ++ "`\""
+                                    ]
+                       , ppOrder = \([workspaces, layout, title, mail]) 
+                                 -> [workspaces, layout, title, mail]
+                       , ppOutput = hPutStrLn xmproc
+                       }
+           , manageHook = manageDocks <+> 
+                          (composeAll $
+                           [ className =? "MPlayer" --> doFloat
+                           , resource =? "Dialog" --> doFloat
+                           , title =? "Pidgin" --> doFloat
+                           , title =? "Dwarf Fortress" --> doFloat
+                           , title =? "QEMU" --> doFloat -- above doesn't work
+                           , className =? "Pidgin" --> doShift "1"
+                           , className =? "Iceweasel" --> doShift "2"
+                           , className =? "Icedove" --> doShift "3"
+                           ]
+                          )
            , handleEventHook = mempty
-           , startupHook = suckterm $ "fortune -a | cowsay -n; linuxlogo" ++ " && " ++ "bash"
-}
+           , startupHook = suckterm $ 
+                           "fortune -a | cowsay -n; linuxlogo" 
+                           ++ " && " ++ "bash"
+           }

23ae7dd2deb2070bd0653111825a25629ab7acc9
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2014-07-08 13:35:02 -0400

    New asm and haskell modes; some notmuch customization.

--- frozencemetery/.emacs.d!init.el
+++ frozencemetery/.emacs.d!init.el
@@ -1,29 +1,36 @@
-;(setq display-time-24hr-format 1)
-;(display-time)
+(add-to-list 'load-path "~/.emacs.d")
+
+(setq display-time-24hr-format 1)
 (column-number-mode)
-;(display-battery-mode)
+
+(require 'tramp)
 
 (require 'auto-complete-config)
 (require 'auto-complete)
+
+(require 'asm-mode)
+(add-to-list 'auto-mode-alist '("\\.S$" . asm-mode))
+
 ;; dirty fix for having AC everywhere
 (define-globalized-minor-mode real-global-auto-complete-mode
   auto-complete-mode (lambda ()
                        (if (not (minibufferp (current-buffer)))
-                         (auto-complete-mode 1))
-                       ))
+                           (auto-complete-mode 1)
+                         )))
 (real-global-auto-complete-mode t)
 (ac-flyspell-workaround)
 (define-key ac-complete-mode-map "\t" 'ac-complete)
 (define-key ac-complete-mode-map "\r" nil)
 
-(load "/home/frozencemetery/.emacs.d/sml-modeline/sml-modeline.el")
-(require 'sml-modeline)
-(sml-modeline-mode)
+(setq x-stretch-cursor nil) ; this seems to be ignored
 
 (defun indent-properly ()
   "Run indent on the buffer"
-  (shell-command-on-region (point-min) (point-max) "indent -" t t))
+  (shell-command-on-region (point-min) (point-max) "indent -" t t)
+  )
 (add-to-list 'auto-mode-alist '("\\.doc\\'" . no-word))
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (add-hook 'c-mode-common-hook
           (lambda ()
@@ -32,26 +39,40 @@
             (set (make-local-variable 'fill-nobreak-predicate)
                  (lambda ()
                    (not (eq (get-text-property (point) 'face)
-                            'font-lock-comment-face))))
-            (indent-properly)
-            ))
-(setq c-default-style "linux" c-basic-offset 2)
-;; I'm actually otbs but eh close enough.
+                            'font-lock-comment-face
+                            ))))))
+(setq c-default-style "linux" c-basic-offset 2) ;; ~OTBS
+(add-hook 'c++-mode-hook ;; stack overflow made me the man I am today
+          (lambda () (setq comment-start "/* " comment-end " */")))
 
-(global-set-key "\C-xp" 'previous-multiframe-window)
-(global-set-key "\C-c;" 'comment-region)
-(global-set-key "\C-c:" 'uncomment-region)
+(remove-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(remove-hook 'haskell-mode-hook 'turn-on-simple-indent)
+(remove-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 
+          (lambda ()
+            (local-set-key (kbd "TAB") '(lambda () (interactive) (insert "  ")))
+            ))
+
 (defun yank-to-x-clipboard ()
   (interactive)
   (if (region-active-p)
-        (progn
-          (shell-command-on-region (region-beginning) (region-end) "xsel -b -i")
-          (message "Yanked region to clipboard!")
-          (deactivate-mark))
-    (message "No region active; can't yank to clipboard!")))
+      (progn
+        (shell-command-on-region
+         (region-beginning)
+         (region-end)
+         "xsel -b -i")
+        (message "Yanked region to clipboard!")
+        (deactivate-mark)
+        )
+    (message "No region active; can't yank to clipboard!")
+    ))
 (global-set-key "\M-W" 'yank-to-x-clipboard)
 
 (setq require-final-newline t) ; hands down best setting
+
+;; this may cause undesirable "" highlighting
+;; it also causes color failure in notmuch-search buffers
+;; (show-ws-toggle-show-trailing-whitespace)
 
 ;; fuck tabs
 (setq-default indent-tabs-mode nil)
@@ -61,6 +82,7 @@
 
 (setq text-mode-hook 'turn-on-auto-fill)
 (setq-default fill-column 78)
+(highlight-beyond-fill-column)
 
 (setq info-mode-hook 'visual-mode)
 
@@ -74,40 +96,24 @@
 (setq org-special-ctrl-a t)
 (setq org-special-ctrl-e t)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(add-to-list 'auto-mode-alist '(".bash_aliases$" . sh-mode))
 
-(defun no-word ()
-  "Run antiword on the entire buffer."
-  (shell-command-on-region (point-min) (point-max) "antiword - " t t))
-(add-to-list 'auto-mode-alist '("\\.doc\\'" . no-word))
+(add-to-list 'auto-mode-alist '(".bash_aliases$" . sh-mode))
 
 (setq completion-ignored-extensions
       (append completion-ignored-extensions
               (list
-               ".bak" 
-               ".CKP" 
-               ".aux" 
-               ".otl" 
-               ".err" 
-               ".lib" 
-               ".dvi" 
-               ".PS" 
-               ".o" 
-               ".pdf" 
+               ".bak"
+               ".CKP"
+               ".aux"
+               ".otl"
+               ".err"
+               ".lib"
+               ".dvi"
+               ".PS"
+               ".o"
+               ".pdf"
                ".log"
                )))
-
-(defun stupid ()
-  "fix goddamn tab completion"
-  (interactive)
-  (pcomplete-std-complete)
-  (just-one-space)
-  (backward-delete-char 1))
-(add-hook 'eshell-mode-hook '(lambda () (define-key eshell-mode-map "\t" 'stupid)))
-(defalias 'e 'find-file)
-(setq eshell-aliases-file "/home/frozencemetery/.emacs.d/eshell-aliases")
-(require 'eshell)
-(eshell)
 
 (setq auto-save-interval 1024)
 
@@ -120,27 +126,6 @@
 
 (load "/home/frozencemetery/.emacs.d/ats-mode.el")
 
-(setq haskell-mode-hook 'turn-on-haskell-indent)
-(defun haskell-shit ()
-  (turn-on-haskell-doc-mode)
-  (defun haskell-electric-| ()
-    (interactive)
-    (haskell-indent-insert-guard)
-    (indent-for-tab-command)
-    )
-  (define-key haskell-mode-map "|" 'haskell-electric-|)
-  (define-key haskell-mode-map (kbd "C-c .") 'haskell-indent-align-guards-and-rhs)
-  (defun haskell-electric-eq ()
-    (interactive)
-    (insert "=")
-    (indent-for-tab-command)
-    )
-  (define-key haskell-mode-map (kbd "=") 'haskell-electric-eq)
-  (setq haskell-stylish-on-save t)
-)
-(add-hook haskell-mode-hook 'haskell-shit)
-(run-haskell)
-
 (global-font-lock-mode 1)
 
 (show-paren-mode 1)
@@ -151,57 +136,48 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(setq TeX-PDF-mode t)
-
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-;; load rudel for *obby
-(defun load-rudel ()
-  (interactive)
-  (add-to-list 'load-path "~/.emacs.d/rudel")
-  (add-to-list 'load-path "~/.emacs.d/rudel/jupiter")
-  (add-to-list 'load-path "~/.emacs.d/rudel/obby")
-  (require 'rudel-mode)
-  (require 'rudel-obby)
-  (global-rudel-minor-mode)
-  (global-rudel-header-subscriptions-mode)
-  (global-rudel-mode-line-publish-state-mode)
-)
+;; ;; load rudel for *obby
+;; (defun load-rudel ()
+;;   (interactive)
+;;   (add-to-list 'load-path "~/.emacs.d/rudel")
+;;   (add-to-list 'load-path "~/.emacs.d/rudel/jupiter")
+;;   (add-to-list 'load-path "~/.emacs.d/rudel/obby")
+;;   (require 'rudel-mode)
+;;   (require 'rudel-obby)
+;;   (global-rudel-minor-mode)
+;;   (global-rudel-header-subscriptions-mode)
+;;   (global-rudel-mode-line-publish-state-mode)
+;; )
 
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(font-latex-math-face ((((class color) (background light)) (:foreground "green"))))
- '(font-latex-verbatim-face ((((class color) (background light)) (:inherit fixed-pitch :foreground "yellow")))))
+(setq TeX-view-program-list
+      '(("zathura" "zathura %o")
+        ))
+(setq TeX-view-program-selection
+      '((output-pdf "zathura")
+        ))
+
 (setq TeX-output-view-style
       (quote
-       (("^pdf$" "." "evince -f %o")
-        ("^html?$" "." "iceweasel %o"))))
+       (("^pdf$" "." "zathura %o")
+        ("^html?$" "." "iceweasel %o")
+        )))
 
-(setq inferior-lisp-program 'sbcl)
+;; (setq inferior-lisp-program 'sbcl)
 ;; (require 'slime)
 ;; (slime)
 ;; (slime-setup '(slime-repl slime-asdf slime-fancy slime-banner))
 
 (setq python-indent 2)
-(run-python)
 
-;; (setq sml-program-name "sml")
-;; ;; fuck you
-;; ;; sml-mode
-;; ;; I want to override your bindings and CAN'T
-;; (global-set-key "\C-c " 'just-one-space)
-;; (global-set-key "\C-c\C- " 'just-one-space)
+(setq sml-program-name "fml")
 
 (setq uniquify-buffer-name-style 'post-forward)
 (require 'uniquify)
 
 (find-file "~/orgmain.org")
-(find-file "~/sched.org")
-;; (run-sml "sml" "sml")
 
 (setq message-send-mail-partially-limit 10000000)
 
@@ -222,16 +198,29 @@
         (let* ((from
                 (save-restriction
                   (message-narrow-to-headers)
-                  (message-fetch-field "from")))
+                  (message-fetch-field "from")
+                  ))
                (account
                 (cond
                  ;; I use email address as account label in ~/.msmtprc
-                 ((string-match "rharwood@club.cc.cmu.edu" from)"rharwood@club.cc.cmu.edu")
-                 ((string-match "rharwood@bu.edu" from)"rharwood@bu.edu")
-                 ((string-match "rharwood@cmu.edu" from)"rharwood@andrew.cmu.edu")
-                 ((string-match "rharwood@andrew.cmu.edu" from)"rharwood@andrew.cmu.edu")
-                 ((string-match "threeoften@gmail.com" from)"threeoften@gmail.com"))))
-          (setq message-sendmail-extra-arguments (list '"-a" account))))))
+                 ((string-match "rharwood@club.cc.cmu.edu" from)
+                  "rharwood@club.cc.cmu.edu")
+                 ((string-match "rharwood@bu.edu" from)
+                  "rharwood@bu.edu")
+                 ((string-match "rharwood@cmu.edu" from)
+                  "rharwood@andrew.cmu.edu")
+                 ((string-match "rharwood@andrew.cmu.edu" from)
+                  "rharwood@andrew.cmu.edu")
+                 ((string-match "threeoften@gmail.com" from)
+                  "threeoften@gmail.com")
+                 )
+                )
+               )
+          (setq message-sendmail-extra-arguments (list '"-a" account))
+          )
+        )
+    )
+  )
 (setq message-sendmail-envelope-from 'header)
 (add-hook 'message-send-mail-hook 'cg-feed-msmtp)
 (setq message-mode-hook 'flyspell-mode)
@@ -239,17 +228,41 @@
   (lambda ()
     "restore old space behavior"
     (interactive)
-    (notmuch-show-next-thread t)))
-;; notmuch.el sets its variables through custom-set-variables 
-;; this is fine, albiet a bit strange
+    (notmuch-show-next-thread t)
+    )
+  )
+(define-key notmuch-show-mode-map "r" 'notmuch-show-reply)
+(define-key notmuch-show-mode-map "R" 'notmuch-show-reply-sender)
+(define-key notmuch-search-mode-map "r" 'notmuch-search-reply-to-thread)
+(define-key notmuch-search-mode-map "R"
+  'notmuch-search-reply-to-thread-sender
+  )
+(setq notmuch-show-tag-macro-alist
+      (list '("d" "-unread")
+            ))
+(setq notmuch-search-tag-macro-alist
+      (list '("d" "-unread")
+            ))
+(define-key notmuch-search-mode-map "d"
+  (lambda () (interactive)
+    (notmuch-search-tag "-unread")
+    (notmuch-search-next-thread)
+    ))
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(haskell-indent-literate-Bird-default-offset 1)
  '(haskell-indent-offset 2)
  '(haskell-indentation-starter-offset 2)
+ '(mm-text-html-renderer (quote lynx))
  '(notmuch-saved-searches (quote (("inbox" . "tag:inbox") ("unread" . "tag:unread"))))
- '(org-agenda-files (quote ("~/schedplan.org")))
+ '(notmuch-show-insert-text/plain-hook (quote (notmuch-wash-convert-inline-patch-to-part notmuch-wash-wrap-long-lines notmuch-wash-tidy-citations notmuch-wash-elide-blank-lines notmuch-wash-excerpt-citations)))
  '(sml-indent-level 2))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
